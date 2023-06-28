960a10e5c6b9ff38edab1c33c1fdeafebff74cd2
Henrique Manduca <henriquemanduca@live.com>
2023-06-28 15:54:42 -0300

    Update nerd fonts

--- nerd_font_install.sh
+++ nerd_font_install.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
+# Main repository
+# https://github.com/ryanoasis/nerd-fonts/
+
 # Install necessary packages
-sudo apt-get install -y curl unzip font-manager
-
-URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.0/Meslo.zip"
-FILE="Meslo.zip"
-
-curl -O "$URL"
-
-if [ $? -ne 0 ]; then
-  echo "Integrity check failed."
-  exit 1
-fi
+sudo apt-get install -y curl unzip #font-manager
 
 mkdir nerd-fonts
 
-unzip "$FILE" -d nerd-fonts/
-rm "$FILE"
+echo "Get Meslo? (y/n)"
+read response
+if [[ "$response" == "y" || "$response" == "Y" ]]; then
+  curl -SL -O "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Meslo.zip"
+  unzip -C Meslo.zip -d nerd-fonts/
+  rm Meslo.zip
+fi
+
+# FiraMono
+echo "Get FiraMono? (y/n)"
+read response
+if [[ "$response" == "y" || "$response" == "Y" ]]; then
+  curl -SL -O "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraMono.zip"
+  unzip FiraMono.zip -d nerd-fonts/
+  rm FiraMono.zip
+fi
+
+# FiraCode
+echo "Get FiraCode? (y/n)"
+read response
+if [[ "$response" == "y" || "$response" == "Y" ]]; then
+  curl -SL -O "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip"
+  unzip FiraCode.zip -d nerd-fonts/
+  rm FiraCode.zip
+fi
 
 echo "Unpacking completed"
