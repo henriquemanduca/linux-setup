4c13f8716da4fe2b8d404271a6499a40ec4390ce
Henrique Manduca <henriquemanduca@live.com>
2024-07-27 14:21:27 -0300

    update on install fonts

--- nerd_font_install.sh
+++ nerd_font_install.sh
@@ -6,14 +6,16 @@
 # Install necessary packages
 sudo apt-get install -y curl unzip #font-manager
 
-mkdir nerd-fonts
+sudo mkdir /usr/share/fonts/truetype/meslo /usr/share/fonts/truetype/firamono /usr/share/fonts/truetype/firacode
 
 echo "Get Meslo? (y/n)"
 read response
 if [[ "$response" == "y" || "$response" == "Y" ]]; then
   curl -SL -O "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Meslo.zip"
-  unzip -C Meslo.zip -d nerd-fonts/
+  mkdir -p nerd-fonts/meslo
+  unzip -C Meslo.zip -d nerd-fonts/meslo
   rm Meslo.zip
+  sudo cp ./nerd-fonts/meslo/*.ttf /usr/share/fonts/truetype/meslo/
 fi
 
 # FiraMono
@@ -21,8 +23,10 @@ echo "Get FiraMono? (y/n)"
 read response
 if [[ "$response" == "y" || "$response" == "Y" ]]; then
   curl -SL -O "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraMono.zip"
-  unzip FiraMono.zip -d nerd-fonts/
+  mkdir -p nerd-fonts/firamono
+  unzip FiraMono.zip -d nerd-fonts/firamono
   rm FiraMono.zip
+  sudo cp ./nerd-fonts/firamono/*.otf /usr/share/fonts/truetype/firamono
 fi
 
 # FiraCode
@@ -30,8 +34,12 @@ echo "Get FiraCode? (y/n)"
 read response
 if [[ "$response" == "y" || "$response" == "Y" ]]; then
   curl -SL -O "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip"
-  unzip FiraCode.zip -d nerd-fonts/
+  mkdir -p nerd-fonts/firacode
+  unzip FiraCode.zip -d nerd-fonts/firacode
   rm FiraCode.zip
+  sudo cp ./nerd-fonts/firacode/*.ttf /usr/share/fonts/truetype/firacode
 fi
+
+fc-cache -fv
 
 echo "Unpacking completed"
