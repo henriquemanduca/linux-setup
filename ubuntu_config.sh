40f4b74eac61af005d18e78c2c73dd5faab56eb0
Henrique M <henrique.manduca@externo.cresol.com.br>
2024-10-31 08:28:27 -0300

    Disable dock emblems

--- ubuntu_config.sh
+++ ubuntu_config.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-overview'
+gsettings set org.gnome.shell.extensions.dash-to-dock show-icons-emblems false
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'focus-minimize-or-previews'
